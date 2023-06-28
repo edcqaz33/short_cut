d9c79d66680472734536373b7f9ff5a48608fb02
edcqaz33 <eddie8784@gmail.com>
2023-06-29 06:55:16 +0800

    add shor_cut check, prevent short cut repeated

--- models/randomURL.js
+++ models/randomURL.js
@@ -1,17 +1,17 @@
-const shortCut = require("./shortCuts")
+const shortCuts = require("./shortCuts")
 
 function randomURL(numOfURL) {
   const baseChars = '0123456789qazwsxedcrfvtgbyhnujmikolpQAZWSXEDCRFVTGBYHNUJMIKOLP'
   // 23456789qazwsxedcrfvtgbyhnujmikolpQAZWSXEDCRFVTGBYHNUJMIKOLP'
   let sURL = ''
-  // do {
-  //   sURL = ''
-  for (let i = 0; i < numOfURL; i++) {
-    sURL += baseChars[Math.floor(Math.random() * baseChars.length)]
+  do {
+    sURL = ''
+    for (let i = 0; i < numOfURL; i++) {
+      sURL += baseChars[Math.floor(Math.random() * baseChars.length)]
+    }
+    console.log('y')
   }
-  //   console.log('y')
-  // }
-  // while (!shortCut.findOne({ cuttedURL: sURL }))
+  while (shortCuts.findOne({ cuttedURL: sURL }))
   return sURL
 }
 
